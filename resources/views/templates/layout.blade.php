0e81f0eed07998a5bcf2d0629bfd4feea43c1556
Nathan Hutchinson <nathan@valueweb.co.nz>
2016-01-26 11:06:43 +1300

    Fix dropdown bug on mobile for SPA

--- resources/views/templates/layout.blade.php
+++ resources/views/templates/layout.blade.php
@@ -65,5 +65,13 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/js/bootstrap.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/js/ie10-viewport-bug-workaround.js"></script>
 <script src="/js/main.js"></script>
+<script>
+    $(document).ready(function () {
+        $("#navbar li a").click(function(event) {
+            // check if window is small enough so dropdown is created
+            $("#navbar").removeClass("in").addClass("collapse");
+        });
+    });
+</script>
 </body>
 </html>
